d1fee79cec9b7ed589b90fa56d739ebc196e4300
GQue - DEVELOPED FRONT END <68132577+fcurrao@users.noreply.github.com>
2022-08-27 10:19:20 -0300

    initial commit
    initial project

---
+++ ecomerce-to-best/src/pages/Detail.js
@@ -0,0 +1,19 @@
+
+import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer'
+
+
+const Detail = ({}) => {
+
+
+
+return (
+<div>
+<ItemDetailContainer/>
+
+</div>
+        
+    )
+     }
+    
+    export default Detail
+
